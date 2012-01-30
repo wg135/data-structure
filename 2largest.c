edada2dbcb94e2e83d4a2691adc87f86aecccfd3
wg135 <gaowei0525@gmail.com>
2012-01-29 20:45:39 -0600

    find the 2 largest element in an array in one loop

---
+++ 2largest.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int twond(int array[], int len);
+
+int main(int argc, char *argv[]){
+
+  int array[] = {1, -2, 3, 10, -4, 7, 2, -5};
+  printf("2largest is %d\n", twond(array, sizeof(array)/sizeof(array[0])));
+  
+  return 0;
+}
+
+int twond(int array[], int len){
+
+  int i;
+  int max, secondmax;
+
+  if((array == NULL)||(len == 0)||(len == 1))
+    exit(1);
+
+  max = secondmax = array[0];
+
+  for(i=1; i<len;i++){
+    if(max < array[i]){
+      secondmax = max;
+      max = array[i];
+    }
+    else
+      if(secondmax < array[i])
+        secondmax = array[i];
+  }
+  
+  return secondmax;
+  
+}
